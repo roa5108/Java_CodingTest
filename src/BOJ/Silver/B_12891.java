c69ef9e1f8da39448e0bb8e5237fcdf1c410e78d
Roa Kim <roa5108@gmail.com>
2025-04-23 00:50:00 +0900

    .

--- src/BOJ/Silver/B_12891.java
+++ src/BOJ/Silver/B_12891.java
@@ -1,9 +1,6 @@
 package BOJ.Silver;
 
-import java.io.BufferedReader;
-
 public class B_12891 {
     public static void main(String[] args) {
-        BufferedReader br=new BufferedReader()
     }
 }
