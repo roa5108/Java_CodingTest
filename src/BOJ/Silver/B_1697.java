376bb21818f53e41f8f3a36acca45056ea87e62b
Roa Kim <roa5108@gmail.com>
2025-05-13 00:32:37 +0900

    B_1697

---
+++ src/BOJ/Silver/B_1697.java
@@ -0,0 +1,22 @@
+package BOJ.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_1697 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+
+    }
+
+    public static int bfs(int loc, int goal) {
+        if (loc == goal) return 0;
+
+    }
+}
